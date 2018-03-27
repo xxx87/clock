a6f59bccc47ae538f3fb9b1da5e7b51e1b020dd1
xxx87 <xxx_87@ukr.net>
2018-03-27 15:18:09 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var hoursHand = document.querySelector('.hour-hand'),
+    minutesHand = document.querySelector('.min-hand'),
+    secondsHand = document.querySelector('.second-hand');
+
+function setDate() {
+    var now = new Date(),
+        seconds = now.getSeconds(),
+        minutes = now.getMinutes(),
+        hours = now.getHours();
+
+    var secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    var minutesDegrees = ((minutes / 60) * 360) + 90;
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    var hoursDegrees = ((hours / 12) * 360) + 90;;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}     
+setInterval(setDate, 1000);
+    
